e449d313b6e18a051dbd65f58307b388719af8fb
Daniele Olmisani <daniele.olmisani@gmail.com>
2024-03-02 07:16:57 +0100

    added 'words' in 'formatter' module

---
+++ src/renders/words.py
@@ -0,0 +1,35 @@
+"""
+"""
+
+__all__ = ["bold", "italic", "code", "highlight", "plain"]
+
+
+#TODO: questi non vengono aggiunti direttamente al documento
+
+from textwrap import fill
+
+
+def bold(text: str) -> str:
+    """Format a piece of text in bold.
+    """
+    return f"**{text}**"
+
+def italic(text: str) -> str:
+    """Format a piece of text in italic.
+    """
+    return f"*{text}*"
+
+def code(text: str) -> str:
+    """Format a piece of text as raw code.
+    """
+    return f"`{text}`"
+
+def highlight(text: str) -> str:
+    """Format a piece using highlight feature.
+    """
+    return f"=={text}=="
+
+def plain(text: str, width=20) -> str:
+    """Format a piece of plain text using provided 'width' as end-of-line limit.
+    """
+    return fill(text, width)
