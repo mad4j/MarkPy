08ae30da6955a4d8c5c69ab39d22d9e2e3edf38b
Daniele Olmisani <daniele.olmisani@gmail.com>
2024-03-08 05:39:28 +0100

    Update sha.py

--- src/markpy/sha.py
+++ src/markpy/sha.py
@@ -4,18 +4,18 @@ import hashlib
 
 if __name__ == '__main__':
 
-    caratteri = '|!"$%&/()=?^+*@#[]-_.,;:<>abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-    it = itertools.product(caratteri, repeat=2)
+    caratteri = '~\'\"\\|!"$%&/()}{=?^+*@#[]-_.,;:<>abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    it = itertools.product(caratteri, repeat=5)
 
-    print((int.from_bytes(hashlib.sha256(b"[k!a").digest())+43) % 10**10)
-    print((int.from_bytes(hashlib.sha256(b"!]xWh").digest())-3) % 10**10)
-    print((int.from_bytes(hashlib.sha256(b"&A:an").digest())-10) % 10**10)
-    print((int.from_bytes(hashlib.sha256(b"=SHAT").digest())+63) % 10**10)
-    print(int.from_bytes(hashlib.sha256(b"#im*E").digest()) % 10**10)
+    #print((int.from_bytes(hashlib.sha256(b"[k!a").digest())+43) % 10**10)
+    #print((int.from_bytes(hashlib.sha256(b"!]xWh").digest())-3) % 10**10)
+    #print((int.from_bytes(hashlib.sha256(b"&A:an").digest())-10) % 10**10)
+    #print((int.from_bytes(hashlib.sha256(b"=SHAT").digest())+63) % 10**10)
+    #print(int.from_bytes(hashlib.sha256(b"#im*E").digest()) % 10**10)
 
     for x in it:
         x = "".join(x)
-        h = hashlib.sha256(bytes(x, "UTF-8")).digest()
+        h = hashlib.md5(bytes(x, "UTF-8")).digest()
         d = (int.from_bytes(h) % 10**10) - 3332149721
-        if abs(d) < 10**2:
+        if abs(d) < 10:
             print(f"{x}: {d}")
