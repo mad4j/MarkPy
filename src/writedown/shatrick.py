87ad1ee09e2eff8c10724385b75df45c8ad25a9d
Daniele Olmisani <daniele.olmisani@gmail.com>
2024-03-29 06:49:19 +0100

    Update shatrick.py

--- src/writedown/shatrick.py
+++ src/writedown/shatrick.py
@@ -1,10 +1,27 @@
 
+
 import os
 import binascii
 import hashlib
+
 import zlib
 from functools import wraps
 from time import time
+
+
+class Chunk:
+    def __init__(self) -> None:
+        self.weight = 0
+        self.key = 0
+        self.residual = []
+
+    def __str__(self) -> str:
+        return f'weight: {self.weight}, key: {self.key:010x} residual: {binascii.hexlify(bytearray(self.residual))}'
+
+    def update(self, weight, key, residual) -> None:
+        self.weight = weight
+        self.key = key
+        self.residual = residual
 
 
 def timing(f):
@@ -18,48 +35,73 @@ def timing(f):
         return result
     return wrap
 
-def zeros(bytes):
+def zeros(values):
     value = 0
-    for b in bytes:
+    for b in values:
         value += b.bit_count()
-    return 8*len(bytes) - value
+    return 8*len(values) - value
 
 
 if __name__ == '__main__':
-    data = os.urandom(32)
+
+    data = os.urandom(64)
+    target = zeros(data)
+    data_z = zlib.compress(data, 9)
+
     print(f'Input data: {binascii.hexlify(bytearray(data))}')
     print(f'zeros: {zeros(data)}')
+    print(f'input compressed len: {len(data_z)}')
 
-    target = zeros(data)
+    ck1 = Chunk()
+    ck2 = Chunk()
 
-    domain = 3
-    max = target
-    result = data
-    head = 0
-    for v in range(1 << 8*domain):
+    d1 = data[0:32]
+    d2 = data[32:64]
 
-        h = hashlib.sha256(v.to_bytes(domain, 'big')).digest()
-        t = bytes(a ^ b for (a, b) in zip(data, h))
+    DOMAIN = 4
 
-        r = zeros(t)
-        if r > max:
-            print(f'value: {v:010x}, zeros: {r} result: {binascii.hexlify(bytearray(t))}')
-            max = r
-            result = t
-            head = v
+    for v in range(1 << 8*DOMAIN):
 
-        if v % 0x1000000 == 0:
-            print(f'{v:010x}: max: {max}')
+        H = hashlib.sha256(v.to_bytes(DOMAIN, 'big')).digest()
 
-    print(f'RESULT: value: {head:010x}, zeros: {max} result: {binascii.hexlify(bytearray(result))}')
+        T1 = bytes(a ^ b for (a, b) in zip(d1, H))
+        T2 = bytes(a ^ b for (a, b) in zip(d2, H))
+
+        W1 = zeros(T1)
+        W2 = zeros(T2)
+
+        if W1 > ck1.weight:
+            ck1.update(W1, v, T1)
+            print(f'UPDATED CK1: {ck1}')
+
+        if W2 > ck2.weight:
+            ck2.update(W2, v, T2)
+            print(f'UPDATED CK2: {ck2}')
+
+        if v % 0x100000 == 0:
+            print(f'{v:010x}: max: {ck1.weight}, {ck2.weight}')
+            
+    print(f'RESULT: {ck1}')
+    print(f'RESULT: {ck2}')
 
     data_z = zlib.compress(data, 9)
-    result_z = zlib.compress(result, 9)
-    print(f'compressed size: data: {len(data_z)}, result: {len(result_z)}')
+    print(f' INPUT: size: {len(data)} compressed size: {len(data_z)}')
 
+    output = bytearray()
+    #for b in ck1.key.to_bytes(DOMAIN, 'big'):
+    #     output.append(b)
+    #for b in ck2.key.to_bytes(DOMAIN, 'big'):
+    #    output.append(b)
+    for b in ck1.residual:
+        output.append(b)
+    for b in ck2.residual:
+        output.append(b)
+
+    output_z = zlib.compress(output, 9)
+    print(f' OUTPUT: size: {len(output)} compressed size: {len(output_z)}')
+    
     with open("input.dat", "wb") as binary_file:
         binary_file.write(data)
 
     with open('output.dat', 'wb') as binary_file:
-        binary_file.write(head.to_bytes(domain, 'big'))
-        binary_file.write(result)
+        binary_file.write(output)
